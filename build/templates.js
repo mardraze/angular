6266b4e706d220f85d77965511a89b821ab183f9
Marcin Drazek <marcindr1@gmail.com>
2017-04-02 19:18:52 +0200

    Customization increase

--- build/templates.js
+++
@@ -1,6 +0,0 @@
-angular.module('ngApp').run(['$templateCache', function($templateCache) {  'use strict';
-
-  $templateCache.put('directive/example.html',
-    "<div ng-bind=\"text\"></div>\n"
-  );
-}]);
